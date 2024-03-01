f9853a03e823634bb347de671d73989178b99c1c
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-02-29 22:56:47 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,10 +7,5 @@ resource "google_container_registry_repository" "my_repo" {
   name = "my-repo-lili"
   project = "lili-devops"
 }
-provider "google-beta" {
-  project = "your-project-id"
-  region  = "your-region"
-}
-
 
 
