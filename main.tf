1beb66737d4a66d8bd96665dee3d26d5c85ca258
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-03-01 18:50:20 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,18 +3,7 @@ provider "google" {
   region  = "us-east4"
 }
 
-data "google_container_registry_repository" "all_repos" {
-  project = "lili-devops"
-}
-
-locals {
-  my_repo = {
-    for repo_name, repo in data.google_container_registry_repository.all_repos : 
-    repo_name => repo
-    if contains(keys(repo), "my-repo-lili")
-  }
-}
-
-output "repository_name" {
-  value = local.my_repo["my-repo-lili"].name
+resource "google_artifact_registry_repository" "my_repository" {
+  name     = "my-repository"
+  location = "us-east4" # Maryland is located in the us-east4 region
 }
