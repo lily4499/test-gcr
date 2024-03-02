748561a3521693b0eeea52e651052cbd7cd8d40d
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-03-01 19:56:02 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "google" {
 }
 
 resource "google_artifact_registry_repository" "my_repository" {
-  repository_id = "my-repository"
+  repository_id = "lili-gcr-repo"
   location      = "us-east4" # Maryland is located in the us-east4 region
   format        = "DOCKER"   # Specify the format of packages (e.g., Docker)
 }
